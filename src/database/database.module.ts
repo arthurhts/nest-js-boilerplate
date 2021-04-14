ed425b50b943893c2ce08917ea4bf2cb2dd24b71
Arthur Teixeira <arthurhenrique@evoluaeducacao.com.br>
2021-04-13 22:09:24 -0300

    alter config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,22 @@
 import { UserEntity } from '../users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.TYPEORM_USERNAME,
-      port: Number(process.env.TYPEORM_HOST),
-      database: process.env.TYPEORM_DATABASE,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
+      host: process.env.DB_HOST,
+      port: 5432,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       entities: [UserEntity],
       synchronize: false,
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
     }),
   ],
 })
